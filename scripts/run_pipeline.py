abc250345a75a70aa0b7c74ed6a36627f6e9ed0b
Rameez Haider <mdrameezhaider@gmail.com>
2025-01-10 22:31:31 +0530

    encoder commit

--- scripts/run_pipeline.py
+++
@@ -1,6 +0,0 @@
-from diffusers import StableDiffusionPipeline
-import torch
-
-model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype = torch.float16)
-pipe.to("cpu")
