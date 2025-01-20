d0e43452c092f1a493cbff7e72bc55820858539d
Rameez Haider <mdrameezhaider@gmail.com>
2025-01-20 13:32:18 +0530

    fix: removed unused dependencies

--- sd/clip.py
+++ sd/clip.py
@@ -1,7 +1,28 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
 from attention import SelfAttention
+
+class CLIP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = CLIPEmbedding(49408, 768, 77)
+
+        self.layers = nn.Module([
+            CLIPLayer(12, 768) for i in range(12)
+        ])
+
+        self.layernorm == nn.LayerNorm(768)
+
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+
+        state = self.embeddings(tokens)
+
+        for layer in self.layers:
+            state = layer(state)
+        output = self.layernorm(state)
+
+        return output
 
 class CLIPEmbedding(nn.Module):
     def __init__(self, n_vocab: int, n_embd: int, n_tokens: int):
@@ -51,23 +72,3 @@ class CLIPLayer(nn.Module):
 
         return x
 
-class CLIP(nn.module):
-    def __init__(self):
-        self.embedding = CLIPEmbedding(49408, 768, 77)
-
-        self.layers = nn.Module([
-            CLIPLayer(12, 768) for i in range(12)
-        ])
-
-        self.layernorm == nn.LayerNorm(768)
-
-    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
-        tokens = tokens.type(torch.long)
-
-        state = self.embeddings(tokens)
-
-        for layer in self.layers:
-            state = layer(state)
-        output = self.layernorm(state)
-
-        return output
