587d03266106fe932aa1998ce4fcaf15514a8d0f
Rameez Haider <mdrameezhaider@gmail.com>
2025-01-22 23:54:13 +0530

    fix: indendation fixes

--- sd/attention.py
+++ sd/attention.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 import math
 
 class SelfAttention(nn.Module):
-    def __init__(self, n_heads: int, d_embed: int, in_proj_bias: True, out_proj_bias: True)
+    def __init__(self, n_heads: int, d_embed: int, in_proj_bias: True, out_proj_bias: True):
         super().__init__()
 
         self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias = in_proj_bias)
@@ -85,3 +85,5 @@ class CrossAttention(nn.Module):
         output = output.view(input_shape)
 
         output = self.out_proj(output)
+
+        return output
